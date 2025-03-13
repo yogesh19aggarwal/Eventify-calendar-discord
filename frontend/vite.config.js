bbf8a0419ab617efccdda0203242aa1b01394142
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-03-13 23:49:50 +0530

    Update vercel.json and vit.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html',  // Make sure your entry point is correct
+    }
+  }
 })
