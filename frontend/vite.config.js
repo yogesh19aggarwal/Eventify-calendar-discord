ee8c64f8dc950b9702be15542198c705051ca426
yogesh19aggarwal <yogesh.19.aggarwal@gmail.com>
2025-03-14 00:09:47 +0530

    update vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
+  optimizeDeps: {
+    include: ["@mui/material", "@mui/icons-material"]
+  },
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',  // Make sure your entry point is correct
+    commonjsOptions: {
+      include: [/node_modules/],
     }
   }
-})
+});
